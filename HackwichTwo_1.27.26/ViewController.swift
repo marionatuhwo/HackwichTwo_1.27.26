178d8ce2dffb610e1c4257359a48ec8a4e7b770c
marionatuhwo <ano@hawaii.edu>
2026-01-27 12:24:25 -1000

    Completed Hackwich Two In Class Practice

--- HackwichTwo_1.27.26/ViewController.swift
+++ HackwichTwo_1.27.26/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func pressMeButtonPressed(_ sender: UIButton)
+    {
+            print("Hello World!")
+    }
+    
+    
+    
+    
+    
+    
 }
 
